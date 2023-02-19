d2f79516592a528e1064078e1034c71bfeb6c0e4
Ajay Ranabhat <ajay.ranabhat19@gmail.com>
2023-02-19 13:59:33 +0545

    Perofprmance pie chart added

--- frontend/src/components/Performance/index.jsx
+++ frontend/src/components/Performance/index.jsx
@@ -4,9 +4,45 @@ import "react-calendar/dist/Calendar.css";
 import Wallpaper from "./../../components/wallpaper";
 import { Grid } from "@material-ui/core";
 import Sidebar from "./../../components/sidebar";
-// import "./course.css";
+import "./index.css";
+import { Chart } from "react-google-charts";
 
 const Index = () => {
+
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Previous Score", 11],
+    ["Attendence Score", 2],
+    ["Assignment Score", 2],
+    ["Assessment Score", 7],
+  ];
+
+  const options = {
+    title: "My Semester Perfromance",
+    is3D: true,
+  };
+
+  // const chartRef = useRef();
+
+  // useEffect(() => {
+  //   const myChartRef = chartRef.current.getContext("2d");
+
+  //   new Chart(myChartRef, {
+  //     type: "pie",
+  //     data: {
+  //       labels: ["Previous Score", "Attendence Score", " Assignment Score", "Assessment Score"],
+  //       datasets: [
+  //         {
+  //           label: "My Chart",
+  //           data: [10, 20, 30, 40],
+  //           backgroundColor: ["#f44336", "#2196f3", "#4caf50", "#ffc107"],
+  //         },
+  //       ],
+  //     },
+  //   });
+  // }, []);
+
+
   return (
     <div className="App">
       <Grid container direction="row" spacing={1}>
@@ -17,7 +53,20 @@ const Index = () => {
           <Wallpaper />
           <Grid container direction="row">
             <Grid item xs={7}>
-              <h1>Coming Soon..</h1>
+              <div className="performance_pieChart">
+                {/* <canvas ref={chartRef} style={{ width: "100%" }} /> */}
+                <Chart
+                  chartType="PieChart"
+                  data={data}
+                  options={options}
+                  width={"100%"}
+                  height={"400px"}
+                />
+
+              </div>
+              <div className="remarks">
+                <h1>Remarks:Excellent</h1>
+              </div>
             </Grid>
             <Grid item xs={5}>
               <Calendar />
@@ -31,3 +80,5 @@ const Index = () => {
 // }
 
 export default Index;
+
+
